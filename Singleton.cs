ccba8e44e0e77986efa3e9737f01f05b01221986
Gil Ferraz <GilFerraz@users.noreply.github.com>
2017-02-13 14:16:45 +0000

    Added the Singleton class

---
+++ Singleton.cs
@@ -0,0 +1,55 @@
+﻿namespace Faxime.Common.Design
+{
+    #region
+
+    using System;
+
+    #endregion
+
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Singleton<T>
+        where T : Singleton<T>
+    {
+        #region Static Fields and Constants
+
+        private static T instance;
+        private static readonly object threadLock = new object();
+
+        #endregion
+
+        #region Proprieties
+
+        #region Public Proprieties
+
+        /// <summary>
+        ///     The instance of this <see cref="Singleton{T}" />.
+        /// </summary>
+        /// <remarks>
+        ///     If the singleton is not initialized, automatically initializes the singleton
+        ///     with its private parameterless constructor.
+        /// </remarks>
+        public static T Instance { get { return GetInstance(); } }
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Private Static Methods
+
+        private static T GetInstance()
+        {
+            lock (threadLock)
+            {
+                return instance ?? (instance = Activator.CreateInstance(typeof(T), true) as T);
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
